c29593024c0d7fbac3f14769f7161567f76927de
Sashfish <dormiii4@gmail.com>
2020-01-31 18:15:18 +0000

    blank project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace musicname
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
